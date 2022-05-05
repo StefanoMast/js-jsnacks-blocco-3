e8a3c12b01a7dd0d0518d65924d325c51c2c6b18
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-05 10:32:16 +0200

    add bonus

---
+++ bonus/script.js
@@ -0,0 +1,23 @@
+// Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+const numbers = [];
+
+for(let i=0; i<10; i++) {
+    numbers.push(getRndInteger(1,100));
+}
+
+console.log(numbers);
+
+const oddContainer = document.getElementById("odd");
+const evenContainer = document.getAnimations("even");
+
+console.log(oddContainer, evenContainer);
+
+numbers.forEach(number => {
+    (number % 2 === 0) ? evenContainer.innerHTML += `$(number)` : oddContainer.innerHTML += `$(number)` ;
+});
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
